b773b0304117d9ea01bfbfb2d55aea3fbe1a882e
Reivaldy-Zaen <reivaldyz@gmail.com>
2025-08-20 21:33:39 +0700

    link pekerjaan

--- resources/views/dashboard/index.blade.php
+++ resources/views/dashboard/index.blade.php
@@ -102,7 +102,7 @@
                 <span class="mr-3 text-xl">📊</span>
                 <span class="font-medium">Dashboard</span>
                 <span class="ml-auto            <a href="{{route('pekerja.index')}}" class="group flex items-center py-3 px-4 rounded-xl transition-all duration-300">
-         <a href="#" class="group flex items-center py-3 px-4 rounded-xl transition-all duration-300">
+         <a href="{{route('pekerja.index')}}" class="group flex items-center py-3 px-4 rounded-xl transition-all duration-300">
 >
                 <span class="mr-3 text-xl">👥</span>
                 <span class="font-medium">Pekerja</span>
